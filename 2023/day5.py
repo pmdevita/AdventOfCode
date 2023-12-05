a025ef0e34e023503ce61395e113acc49c58c055
Peter DeVita <mewtwo2643@yahoo.com>
2023-12-05 01:51:12 -0500

    day 5 cleanup

--- 2023/day5.py
+++ 2023/day5.py
@@ -10,13 +10,6 @@ def get_nums(s):
     return [int(i.strip()) for i in re.findall(r" \d+", s)]
 
 
-seeds = get_nums(sections[0][6:])
-
-seeds2 = []
-for i in range(0, len(seeds), 2):
-    seeds2.append((0, seeds[i], seeds[i + 1]))
-
-
 def get_map(section):
     rules = []
     for line in section.split("\n")[1:]:
@@ -33,24 +26,6 @@ def translate_num(number: int, rules: list):
     return number
 
 
-def reverse_translate_num(number: int, rules: list):
-    for i, r in enumerate(rules):
-        if r[1] <= number < r[1] + r[2]:
-            return r[0] + (number - r[1])
-    return number
-
-
-def in_seeds(num):
-    for r in seeds2:
-        if r[1] <= num < r[1] + r[2]:
-            return True
-    return False
-
-
-maps = [get_map(s) for s in sections[1:]]
-reversed_maps = reversed(maps)
-
-
 def get_seed_locations(seeds):
     locations = []
     for seed in seeds:
@@ -61,15 +36,7 @@ def get_seed_locations(seeds):
     return locations
 
 
-def reverse_locations(location):
-    num = location
-    for map in reversed_maps:
-        num = reverse_translate_num(num, map)
-    return in_seeds(num)
-
-
 def find_paths(rules1: list, rules2: list):
-    has_edited = True
     source_rules = rules1.copy()
     usable_r2 = set()
     while source_rules:
@@ -84,7 +51,7 @@ def find_paths(rules1: list, rules2: list):
         # Rewrite rule inner
         r_start = max(r[1], r2[0])
         r_end = min(r[1] + r[2], r2[0] + r2[2])
-        rule = (r_start,  r_start - r2[0] + r2[1], r_end - r_start)
+        rule = (r_start, r_start - r2[0] + r2[1], r_end - r_start)
         usable_r2.add(rule)
 
         if r[1] < r_start < r[1] + r[2]:
@@ -96,8 +63,15 @@ def find_paths(rules1: list, rules2: list):
     return list(usable_r2)
 
 
+seeds = get_nums(sections[0][6:])
+
+seeds2 = []
+for i in range(0, len(seeds), 2):
+    seeds2.append((0, seeds[i], seeds[i + 1]))
+
+maps = [get_map(s) for s in sections[1:]]
+
 locations = get_seed_locations(seeds)
-# locations2.sort()
 locations.sort()
 print(locations[0])
 
@@ -106,12 +80,5 @@ for map in maps:
     current = find_paths(current, map)
 
 current.sort(key=lambda x: x[1])
-# print(current)
 
-for i in range(current[0][1], current[0][1] + current[0][2]):
-    # if i % 100_000 == 0:
-    #     print(i)
-    if reverse_locations(i):
-        print(i)
-        break
-# print(locations2[0])
+print(current[0][1])
